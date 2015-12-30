3902433ab1bf270ef4f6994359e123c0da3f5004
Alexandre <alex_balde12@hotmail.com>
2015-12-30 00:15:09 +0000

    Update p067.fsx

--- FSharp/p067.fsx
+++ FSharp/p067.fsx
@@ -3,7 +3,7 @@ let p_067 =
   let helper (s1: seq<int16>) (s2: seq<int16>) = 
     let newS2 = s2 |> Seq.pairwise |> Seq.map System.Math.Max
     Seq.map2 (+) s1 newS2 
-  System.IO.File.ReadAllLines("C:\\Users\\Asus\\Desktop\\Euler\\p067.txt") |>
+  System.IO.File.ReadAllLines("p067.txt") |> (*this should be the filepath where the problem's file is*)
   Seq.map (fun x -> x.Split [|' '|] |>
   Seq.map int16) |>
   Seq.reduceBack helper |> 
